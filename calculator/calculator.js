c8feed729dbac06747af15cc5135e43a53a1362a
arelymartinez16 <arelymartinez741@gmail.com>
2023-06-08 19:07:29 -0400

    Done with making mini calculator with basic functionalities

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,9 +3,17 @@ function display(data) {
     return data;
 }
 
-function solve() {
+function onResult() {
+    console.log("solveEquals function was clicked")
     let screen = document.getElementById("screen")
     let data = screen.value;
-    let result = evalf(data);
-    return result;
+    let result = eval(data);
+    screen.value = result;
+    return result.toString();
+}
+
+function clearScreen() {
+    let screen = document.getElementById('screen');
+    screen.value = "";
+    console.log("Clear button was clicked")
 }
