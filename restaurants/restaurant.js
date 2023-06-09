db0605df607a9dfeb72ed7dfc631fd9f9852a629
arelymartinez16 <arelymartinez741@gmail.com>
2023-06-09 19:15:42 -0400

    Created restaurant.js to create a class and constructor

---
+++ restaurants/restaurant.js
@@ -0,0 +1,8 @@
+class Restaurant {
+    constructor(name, rating, numEmployees, menu) {
+        this.name = name;
+        this.rating = rating;
+        this.numEmployees = numEmployees;
+        this.menu = menu;
+    }
+}
